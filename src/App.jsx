cc2a8de3ad593804ef57adeb0a334f51f28c0958
Guilherme Sofiati Fantin <gui_sf@hotmail.com>
2023-06-07 09:51:35 -0300

    components and properties

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { Post } from "./Post"
+
+function App() {
+  return (
+    <>
+      <h1>Hello, world!</h1>
+      <Post
+        author="Jose"
+        content="Lorem ipsum dolor, sit amet consectetur adipisicing elit."
+      />
+      <Post
+        author="Gabriel"
+        content="Lorem ipsum dolor, sit amet consectetur adipisicing elit."
+      />
+    </>
+  )
+}
+
+
+export default App
---
+++ src/Post.jsx
@@ -0,0 +1,13 @@
+// props: { author: "", content: "" }
+
+export function Post(props) {
+  console.log(props);
+  return (
+    <div>
+      <strong>
+        <p>{props.author}</p>
+      </strong>
+      <p>{props.content}</p>
+    </div>
+  )
+}
